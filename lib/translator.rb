b538fc807f93da2992dc9490adb70a1d1f76eefb
Ruby Weiner <rubyweiner796@me.com>
2018-06-19 22:12:19 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(library)
+ new_hash = {
+   "get_meaning" => {},
+   "get_emoticon" => {}
+ }
+ 
+ library.each do |meaning, translation|
+   english = translation[0]
+   japanese = translation[1]
+   new_hash["get_meaning"][japanese] = meaning
+   new_hash["get_emoticon"][english] = japanese
+ end
+  return new_hash
 end
 
 def get_japanese_emoticon
